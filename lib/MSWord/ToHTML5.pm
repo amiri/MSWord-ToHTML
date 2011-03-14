d351f1db436a79d7efb9e9d5fdfc2e90f593096d
Amiri Barksdale at Home <amiri@roosterpirates.com>
2011-03-14 09:35:44 -0700

    First commit

---
+++ lib/MSWord/ToHTML5.pm
@@ -0,0 +1,88 @@
+package MSWord::ToHTML5;
+
+use Moose;
+use feature "say";
+use MooseX::Method::Signatures;
+use IO::All;
+use IO::All::File;
+use Carp;
+use Archive::Zip qw/:ERROR_CODES :CONSTANTS/;
+use Archive::Zip::MemberRead;
+use autodie;
+use File::Spec;
+use namespace::autoclean;
+use Path::Class::Dir;
+use File::Path qw/make_path/;
+use File::Basename;
+use XML::LibXML;
+use XML::LibXSLT;
+
+has 'parser' => (
+  is => 'ro',
+  isa => 'XML::LibXML',
+  lazy => 1,
+  default => sub {
+    XML::LibXML->new
+  },
+);
+has 'transformer' => (
+  is => 'ro',
+  isa => 'XML::LibXSLT',
+  lazy => 1,
+  default => sub {
+    XML::LibXSLT->new
+  },
+);
+
+has 'style' => (
+  is => 'ro',
+  lazy => 1,
+  default => sub {
+    my $self = shift;
+    $self->parser->load_xml(location=>'etc/docx2html-MS.xsl', no_cdata=>1)
+  },
+);
+
+method validate_file($file) {
+    return
+      unless $file;
+
+    my $dir_name = basename($file);
+    say "My dir_name is: $dir_name";
+
+    open( STDOUT, '>', File::Spec->devnull );
+    my $path = Path::Class::Dir->new( 'tmp', $dir_name );
+    $path->mkpath;
+
+    system( 'antiword', $file );
+    if ( ${^CHILD_ERROR_NATIVE} == 0 ) {
+      system("antiword -m 8859-1 -x db $file > $path/document.xml");
+      return io("$path/document.xml");
+    }
+
+    my $unzip = Archive::Zip->new;
+    if ( $unzip->read($file) == AZ_OK ) {
+      say "I will extract $file to $path";
+      $unzip->extractTree( "", "$path/" );
+      return io("$path/word/document.xml");
+    }
+    else {
+      say "I could not read $file";
+    }
+
+    confess "I don't know what to do with this file";
+}
+
+method get_dom ($file) {
+  return $self->parser->parse_file($file);
+}
+
+method convert_to_html ($dom) {
+  my $stylesheet = $self->transformer->parse_stylesheet($self->style);
+  my $results = $stylesheet->transform($dom);
+  return $stylesheet->output_as_bytes($results);
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ t/extract_html.t
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Devel::Dwarn;
+use Test::Most qw/no_plan/;
+use lib 'lib';
+use MSWord::ToHTML5;
+use Archive::Zip;
+use IO::All;
+
+my @docs  = glob('t/data/*.doc');
+my @docxs = glob('t/data/*.docx');
+
+my $converter;
+
+my @type_methods     = qw/to_File/;
+my @instance_methods = qw/validate_file/;
+
+lives_ok { $converter = MSWord::ToHTML5->new } "My constructor works OK";
+
+#can_ok( $converter, @type_methods );
+can_ok( $converter, @instance_methods );
+
+for my $doc (@docs) {
+    my ( $response, $xml_dom,$html );
+    lives_ok { $response = $converter->validate_file($doc) }
+    "My recognize functions works OK for docs";
+    lives_ok { $xml_dom = $converter->get_dom($response) } "My xml_dom is OK";
+    lives_ok { $html = $converter->convert_to_html($xml_dom) } "My html is OK";
+    diag Dwarn $html;
+}
+
+for my $docx (@docxs) {
+    my ( $response, $xml_dom,$html );
+    lives_ok { $response = $converter->validate_file($docx) }
+    "My recognize functions works OK for docxs";
+    lives_ok { $xml_dom = $converter->get_dom($response) } "My xml_dom is OK";
+
+    lives_ok { $html = $converter->convert_to_html($xml_dom) } "My html is OK";
+    #diag Dwarn $html;
+}
+
+#lives_ok { $converter->convert_doc($_) } "My converter works OK" for @docs;
+
+#throws_ok { $converter->convert_doc($_) } qr/This is not a valid Word doc file/ for @docxs;
+
+#lives_ok { $converter->convert_docx($_) } "My converter works OK" for @docxs;
+#throws_ok { $converter->convert_docx($_) } qr/That does not appear to be a Word docx file/ for @docs;
+
+#my @zips = map { Archive::Zip->new($_) } @docxs;
+#my @files = map { $_->memberNamed('word/document.xml') } @zips;
+
+#diag Dwarn \@files;
+
